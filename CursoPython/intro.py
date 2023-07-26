4dbd12dca168ffa3ba2a0c15085954f93f478ba9
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2023-07-25 18:55:55 -0600

    Ejercicios agregados

---
+++ CursoPython/intro.py
@@ -0,0 +1,6 @@
+"""Introducción a Python"""
+print("Hola mundo!")
+print("El weta " * 4)
+
+THISISATEST = "okok"
+print(THISISATEST)
---
+++ CursoPython/tipos/01-variables.py
@@ -0,0 +1,7 @@
+nombre_curso = "Ultimate Python"
+NombreCurso = "Ultimate Python"
+print(nombre_curso, NombreCurso)
+print(len(nombre_curso))
+print(nombre_curso[0])
+print(nombre_curso[0:9])
+print(nombre_curso[:8])
