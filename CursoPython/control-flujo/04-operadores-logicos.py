9e91d215dbc85fa0c951f435cbe760c63cd6dc36
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2023-08-16 21:31:46 -0600

    Operadores

---
+++ CursoPython/control-flujo/04-operadores-logicos.py
@@ -0,0 +1,10 @@
+# and, or, not
+gas = not True
+encendido = not True
+
+if not gas and not encendido:
+    print("Puedes avanzar")
+
+# Operador de corto circuito, si estamos trabajando con and
+# y la primera
+# Si hay una expresion que es falsa en and, la expresión de la derecha no se ejecutará y con or pues si
---
+++ CursoPython/control-flujo/05-cadena-comparadores.py
@@ -0,0 +1,4 @@
+edad = 25
+
+if 15 <= edad <= 24:
+    print("Puede entrar a la alberca")
